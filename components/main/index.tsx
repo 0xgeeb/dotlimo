2a0fe024c0774ea954dda577b3bb2c140294a7bc
0xgeeb <0xgeeb@gmail.com>
2023-08-04 15:02:40 -0400

    fixed twitter view

--- components/main/index.tsx
+++ components/main/index.tsx
@@ -5,7 +5,7 @@ export const Main = () => {
     <div className="h-[85%] bg-repeat bg-[url('/bg_main.png')]">
       <div className="h-[100%] mx-[5%] lg:mx-[18%] font-inter">
         <TopMain />
-        <div className="w-[100%] h-[50%] flex flex-col sm:flex-row sm:justify-around pt-[3%] sm:pt-[8%] max-[400px]:h-[65%]">
+        <div className="w-[100%] h-[50%] flex flex-col sm:flex-row sm:justify-around sm:pt-[8%] max-[400px]:h-[65%]">
           <Projects />
           <Socials />
         </div>
--- components/topMain/index.tsx
+++ components/topMain/index.tsx
@@ -5,7 +5,7 @@ export const TopMain = () => {
         <img className="rounded-xl" src="./main_pic.png" alt="bg" />
         <p className="text-slate-300 pt-[3%]">yo im geeb.eth I like building cool shit</p>
       </div>
-      <div className="w-[30%] sm:w-[50%] my-auto mx-auto pl-[0%] sm:pl-[10%] lg:pl-[0%] max-[400px]:hidden">
+      <div className="w-[30%] sm:w-[50%] my-auto mx-auto hidden sm:block sm:pl-[10%] lg:pl-[0%]">
         <img className="mx-auto" src="./geebanimals.png" alt="animals" />
         <a href="https://github.com/0xgeeb" target="blank"><div className="bg-[#eb8d29] text-[1.3rem] text-center text-white py-1 w-[80%] sm:w-[40%] lg:w-[25%] mx-auto mt-[10%] sm:mt-[5%] rounded-md hover:scale-125 cursor-pointer" id="github-button">github</div></a>
       </div>
